a359904b3ac40493c292abd602ef7e1582662eb4
Andre Sanches <andrecsanches2@gmail.com>
2022-07-08 11:55:08 -0300

    iterations 1, 2 Done

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 
 const PORT = 4000;
 
 // Importing all the pokemon for our data file
 const allPokemon = require("./data");
+const { application } = require("express");
 
 const app = express();
+app.use(bodyParser.json())
 
 // -- Define your route listeners here! --
+app.get('/pokemon', (req, res) => {
+    console.log(req)
+    res.send(allPokemon)
+})
+
+app.get('/pokemon/:id', (req, res) => {
+    const {id} = req.params
+    res.send(allPokemon[id])
+})
+
+// app.get(`/search`, (req, res) => {
+//     res.send(allPokemon)
+// })
+
 
 app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
